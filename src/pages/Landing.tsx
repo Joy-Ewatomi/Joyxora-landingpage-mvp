808e904ac67f92f0abe53a1738ca0ad79729e0a0
joy-Ewatomi <joravytech@gmail.com>
2025-10-08 10:45:27 +0100

    here again mvp begins

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import {motion} from 'framer-motion'
 
+import React from 'react';
+import { motion } from 'framer-motion';
 
-const Landing: React.FC = () =>  {
- return (
-   <section w-full h-full bg-joyxora-dark text-joyxora-textMuted>
-  <div grid grid-col md-row text-joyxora-green px-12 py-12 text-semibold>
-<h1>hello world</>
-</div>
-</section>
-);
+const Landing: React.FC = () => {
+  return (
+    <section className="w-full h-full bg-joyxora-dark text-joyxora-textMuted">
+      <div className="grid grid-cols-1 md:grid-cols-2 text-joyxora-green px-12 py-12 font-semibold">
+        <h1>Hello world</h1>
+      </div>
+    </section>
+  );
 };
+
+export default Landing;
