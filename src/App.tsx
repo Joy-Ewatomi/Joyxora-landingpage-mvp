e8d83448c8c542c67813f5b1c21c6e2c73b20cc3
joy-Ewatomi <joravytech@gmail.com>
2025-10-11 20:16:20 +0100

    corrected errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Features from "./components/Features";
 import Comparison from "./components/Comparison";
 import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 // Home page component
 const Home: React.FC = () => {
@@ -20,12 +22,13 @@ const Home: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/landing" element={<Landing />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 };
 
 export default App;
-
