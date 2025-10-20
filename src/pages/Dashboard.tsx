0a7ecb764dac3f29ef4da531a97ce7b1ef1c385d
joy-Ewatomi <joravytech@gmail.com>
2025-10-20 14:07:36 +0100

    updated dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FileText, Lock, MessageSquare, Bot, Terminal, Shield, User, Archive, Settings } from 'lucide-react';
+import {  Lock, MessageSquare, Bot, Terminal, Shield, Archive, Settings } from 'lucide-react';
 import FileEncryption from "../components/FileEncryption.tsx";
 
 type FeatureType = 'files' | 'apps' | 'messages' | 'chatbot' | 'terminal' | 'vault' | 'profile';
